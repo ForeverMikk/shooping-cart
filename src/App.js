ccdca2e87968a7987df22c0993da0cda8bfe80d3
Michael Molina <mi1mo2e3@gmail.com>
2022-04-28 20:50:10 -0500

    add new UI slice and its functionality

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React, { useEffect } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import axios from 'axios';
 
 import "./App.css";
 import Auth from "./components/Auth";
 import Layout from "./components/Layout";
+import Notification from "./components/Notification";
+import { uiActions } from "./store/ui-slice";
 
 
 
 function App() {
 
+  const dispatch = useDispatch();
+  const notification = useSelector(state => state.ui.notification);
   const cart = useSelector(state => state.cart);
+
+
 
   const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
   console.log("Is Logged In", isLoggedIn)
@@ -30,24 +36,48 @@ function App() {
 
   // using axios
   useEffect(() => {
-    async function setItemsCart(){
+    // Send state as a request
+    async function sendRequest(){
       try {
+
+        dispatch(uiActions.showNotification({
+          open: true,
+          type: 'warning',
+          message: 'Sending Request'
+        }))
+
         const response = await axios.put('https://redux-http-38b3d-default-rtdb.firebaseio.com/cartItems.json',
           JSON.stringify(cart)
         )
-        console.log(response)
+
+        dispatch(uiActions.showNotification({
+          open: true,
+          type: 'success',
+          message: "Sent Request to database Successfully"
+        }))
+
+        const data = response.JSON();
+        console.log("DATA", data)
       } catch(error) {
-        console.log(error)
+
+        dispatch(uiActions.showNotification({
+          open:'true',
+          type: 'error',
+          message: "Sending Request Failed"
+        }))
+
+        // console.log(error)
       }
     }
 
-    setItemsCart()
+    sendRequest()
   }, [cart])
 
 
 
   return (
     <div className="App">
+      {notification && <Notification type={notification.type} message={notification.message} />}
       { !isLoggedIn && <Auth /> }
       { isLoggedIn && <Layout /> }
     </div>
